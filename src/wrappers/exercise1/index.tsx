033e77253975fcf97c5148273130782f2bc892e9
AfonsoMartinho <am.devlops@gmail.com>
2023-04-03 15:24:30 +0100

    feat: exercise1 done

--- src/wrappers/exercise1/index.tsx
+++ src/wrappers/exercise1/index.tsx
@@ -1,36 +1,54 @@
-import React, { Children, useState } from 'react'
+import React, { useState } from 'react'
 
 export const Exercise1 = () => {
   const rootClassName = 'exercise1'
-  const [xPosition, setXPosition] = useState(0);
-  const [yPosition, setYPosition] = useState(0);
-  const [circles, setCircles] = useState<JSX.Element[]>()
+  const [circles, setCircles] = useState<JSX.Element[]>([])
+  const [previousCircles, setPreviousCircles] = useState<JSX.Element[]>([])
 
   const createCircle = (xPos: number, yPos: number) => {
-    setCircles(
-      [
-       (<div 
-        className={`${rootClassName}__circle`} 
-        style={{
-          left: `${xPos}px`,
-          top: `${yPos}px`,
-        }}>
-          <h1>CIRCLE</h1>
-      </div>)
-      ]
-    )
+    const circleRadius = 30;
+    const newCircle = (
+      <div
+        key={`circle-${xPos}-${yPos}`} 
+          className={`${rootClassName}__circle`} 
+          style={{
+            width: `${circleRadius * 2}px`,
+            height: `${circleRadius * 2}px`,
+            left: `${xPos - circleRadius}px`, // decrising the radius of the circle so that it appears on the center of the cursor instead of all the way to the left
+            top: `${yPos - circleRadius}px`, // decrising the radius of the circle so that it appears on the center of the cursor instead of all the way to the top
+          }}>
+      </div>
+    );
+    setCircles([...circles, newCircle]);
+    setPreviousCircles([...circles, newCircle]);
   } 
 
 
   const handleMouseClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    setXPosition(event.clientX);
-    setYPosition(event.clientY);
     createCircle(event.clientX, event.clientY);
   }
+
+  const undoHandler = () => {
+    setCircles((current) => {
+      return current.slice(0, -1)
+    })
+  }
+  
+
+  const redoHandler = () => {
+   setCircles([
+    ...circles, previousCircles[circles.length] 
+   ])
+  }
+
   return (
     <section className={rootClassName}>
+      <h1>Click on the screen to create circles</h1>
+      <div className='buttons'>
+        <button onClick={undoHandler} disabled={circles.length === 0}>Undo</button>
+        <button disabled={previousCircles.length === circles.length} onClick={redoHandler}>Redo</button>
+      </div>
       <div className={`${rootClassName}__clickable-area`} onClick={handleMouseClick}>
-          {xPosition},{yPosition}
           {circles}
       </div>
     </section>
