3cbc85b62ab63920734a674d2c37a2fd35b7adb5
AfonsoMartinho <am.devlops@gmail.com>
2023-04-03 18:34:53 +0100

    feat: exercise2 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './app.gb.scss';
 import { Exercise1 } from './wrappers/exercise1';
+import { Exercise2 } from './wrappers/exercise2';
 
 
 function App() {
   return (
     <div className="react-playground">
-      <Exercise1 />
+      {/* <Exercise1 /> */}
+      <Exercise2 />
     </div>
   )
 }
